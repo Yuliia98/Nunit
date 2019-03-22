6e0c12c20bbfc966739204e51982afeacb59c4fc
Yuliia Shyshman <Yuliia_Shyshman@epam.com>
2019-03-22 15:33:05 +0200

    Initial Commit

--- Fruits.Tests/BaseFruitTests.cs
+++ Fruits.Tests/BaseFruitTests.cs
@@ -4,15 +4,18 @@ using System;
 
 namespace Fruits.Tests
 {
-    [TestFixture]
+    [TestFixture ,Category("base store")]
     public class BaseFruitTests
     {
-        [Test]        
+        [Test]
         public void ShouldGetFruitsPriceByWeight() {
 
             var fruit = new BaseFruit(2.5, 12, "someFruit", "black", 10);
 
             var actualPriceByWeight = fruit.GetFruitsPriceByWeight();
+
+            Assert.That(actualPriceByWeight, Is.EqualTo(25));
+            Assert.That(actualPriceByWeight, Is.LessThanOrEqualTo(25));
 
             Assert.AreEqual(actualPriceByWeight, 25);            
         }
@@ -54,5 +57,27 @@ namespace Fruits.Tests
 
             Assert.AreEqual(actualPriceByWeight, 30);
         }       
+
+        [Test]
+        public void ShouldGetFruitsTaxedPriceByAmount()
+        {
+            var fruit = new BaseFruit(2.5, 12, "someFruit", "black", 10);
+
+            var actualTaxedPrice = fruit.GetFruitsTaxedPriceByAmount(3.5);
+            Assert.AreEqual(42, actualTaxedPrice);
+            Assert.That(actualTaxedPrice, Is.EqualTo(42));
+
+        }
+
+        [Test]
+        public void ShouldGetGetPriceWithTax()
+        {
+            var fruit = new BaseFruit(2.5, 12, "someFruit", "black", 10);
+
+            var actualTaxedPrice = fruit.GetPriceWithTax(2.5);
+            Assert.AreEqual(35, actualTaxedPrice);
+            Assert.That(actualTaxedPrice, Is.EqualTo(35));
+
+        }
     }
 }
